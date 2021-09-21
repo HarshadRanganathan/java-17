4645757d1dadb8aff7a790440a47a8b4d4405c51
HarshadRanganathan <rharshad93@gmail.com>
2021-09-21 23:14:26 +0100

    Text Blocks

---
+++ app/src/main/java/example/TextBlocks.java
@@ -0,0 +1,10 @@
+package example;
+
+public class TextBlocks {
+
+    public static String getTextBlocks() {
+        return """
+                <h1>Text Blocks</h1>
+                """;
+    }
+}
